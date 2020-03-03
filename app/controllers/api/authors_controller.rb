5aec3b4cf3da8eabce449e22e28feeefca50b4b3
hatsu38 <hajiwata0308@gmail.com>
2020-03-04 01:07:58 +0900

    ControllerにRubocop適用

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-class Api::AuthorsController < ApplicationController
-  PER = 10
-  def index
-    @authors = Author.joins(:books).includes(:books).page(params[:page]).per(PER).order(books_count: :desc)
-  end
+module API
+  class AuthorsController < ApplicationController
+    PER = 10
+    def index
+      @authors = Author.joins(:books).includes(:books).page(params[:page]).per(PER).order(books_count: :desc)
+    end
 
-  def show
-    @author = Author.find_by(name: params[:name])
-    @books =  if @author
-                @author.books
-                  .includes(:author, :rakuten_book_info, :ranking)
-                  .order(Arel.sql('rankings.rank is null, rankings.rank asc'))
-                  .order('rakuten_book_infos.medium_image_url desc')
-                  .page(params[:page]).per(PER)
-              else
-                nil
-              end
+    def show
+      @author = Author.find_by(name: params[:name])
+      @books =  if @author
+                  @author.books.
+                    includes(:author, :rakuten_book_info, :ranking).
+                    order(Arel.sql('rankings.rank is null, rankings.rank asc')).
+                    order('rakuten_book_infos.medium_image_url desc').
+                    page(params[:page]).per(PER)
+                end
+    end
   end
 end
--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,63 +1,65 @@
 # frozen_string_literal: true
 
-class Api::BooksController < ApplicationController
-  require 'zip'
-  require 'csv'
-  require 'open-uri'
+module Api
+  class BooksController < ApplicationController
+    require 'zip'
+    require 'csv'
+    require 'open-uri'
 
-  PER = 20
-  def index
-    @books = Book.viewable.efficiency_list.page(params[:page]).per(PER)
-  end
-
-  def show
-    @book = Book.find_by(title: params[:title])
-    return @words = nil if @book.nil?
-
-    page = params[:page].to_i
-    txt_path = "db/txt/#{@book.txt_file}"
-    download_txt(@book.zip_url, txt_path) unless File.exist?(txt_path)
-    File.open(txt_path, 'r') do |f|
-      @words = f.read.split(/(?<=。)/)[page..(page+5)]
+    PER = 20
+    def index
+      @books = Book.viewable.efficiency_list.page(params[:page]).per(PER)
     end
-  rescue StandardError => e
-    logger.warn(e.inspect)
-  end
 
-  def search
-    @books = Book.viewable.search(params[:title])
-  end
+    def show
+      @book = Book.find_by(title: params[:title])
+      return @words = nil if @book.nil?
 
-  def ranking
-    @books = Book.viewable.joins(:ranking, :rakuten_book_info, :author).order('rank').page(params[:page]).per(PER)
-  end
+      page = params[:page].to_i
+      txt_path = "db/txt/#{@book.txt_file}"
+      download_txt(@book.zip_url, txt_path) unless File.exist?(txt_path)
+      File.open(txt_path, 'r') do |f|
+        @words = f.read.split(/(?<=。)/)[page..(page + 5)]
+      end
+    rescue StandardError => e
+      logger.warn(e.inspect)
+    end
 
-  def download_txt(zip_url, txt_path)
-    base_dir = 'db/txt/'
-    URI.parse(zip_url).open do |file|
-      Zip::File.open_buffer(file.read) do |zip|
-        zip.each do |entry|
-          next unless entry.name.end_with?('.txt')
+    def search
+      @books = Book.viewable.search(params[:title])
+    end
 
-          save_path = entry.name.delete('.txt') + '/' + entry.name
-          Dir.mkdir(base_dir + entry.name.delete('.txt'))
-          zip.extract(entry, base_dir + save_path) { true }
+    def ranking
+      @books = Book.viewable.joins(:ranking, :rakuten_book_info, :author).order('rank').page(params[:page]).per(PER)
+    end
+
+    def download_txt(zip_url, txt_path)
+      base_dir = 'db/txt/'
+      URI.parse(zip_url).open do |file|
+        Zip::File.open_buffer(file.read) do |zip|
+          zip.each do |entry|
+            next unless entry.name.end_with?('.txt')
+
+            save_path = entry.name.delete('.txt') + '/' + entry.name
+            Dir.mkdir(base_dir + entry.name.delete('.txt'))
+            zip.extract(entry, base_dir + save_path) { true }
+          end
         end
       end
+      %x("bash `nkf -w -Lu --overwrite #{txt_path}`")
+      txt_fix_format(txt_path)
     end
-    %x("bash `nkf -w -Lu --overwrite #{txt_path}`")
-    txt_fix_format(txt_path)
-  end
 
-  private
+    private
 
-  def txt_fix_format(path)
-    buffer = File.open(path, 'r', &:read)
-    # バックアップ用ファイルを開いて、バッパを書き込む（バックアップ作成）
-    File.open("#{path}.bak", 'w') { |f| f.write(buffer) }
-    # bufferの中身を変換
-    text = buffer.gsub(/--{20,}.[\s\S]+?--{20,}/, '').gsub(/｜/, '').gsub(/《.+?》/, '').gsub(/［.+?］/, '').gsub(/\n(\s|　)*\n/, "\n")
-    # bufferを元のファイルに書き込む
-    File.open(path, 'w') { |f| f.write(text) }
+    def txt_fix_format(path)
+      buffer = File.open(path, 'r', &:read)
+      # バックアップ用ファイルを開いて、バッパを書き込む（バックアップ作成）
+      File.open("#{path}.bak", 'w') { |f| f.write(buffer) }
+      # bufferの中身を変換
+      text = buffer.gsub(/--{20,}.[\s\S]+?--{20,}/, '').gsub(/｜/, '').gsub(/《.+?》/, '').gsub(/［.+?］/, '').gsub(/\n(\s|　)*\n/, "\n")
+      # bufferを元のファイルに書き込む
+      File.open(path, 'w') { |f| f.write(text) }
+    end
   end
 end
